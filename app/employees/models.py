351678f36fb75ac6bd2679a60de9a19a72756662
Fedorov <atiliogoulart@gmail.com>
2021-08-25 10:27:49 -0300

    Add models employee

--- app/employees/models.py
+++ app/employees/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class EmployeeModel(models.Model):
+
+    name = models.CharField('Name', max_length=50, null=True)
+    age = models.IntegerField('Age', default=18)
+    password = models.CharField('Password', max_length=50)
+
+
+    def __str__(self):
+        return self.name
